a5b112d167ee6d628f264247d778a5dc795cf0c0
kimsiwoo <ksw98035213@gmail.com>
2026-01-22 14:55:49 +0900

    feat :: 테마 프로바이더 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { Global } from '@emotion/react';
-import { GlobalStyle } from '@/styles/theme';
+import { Global, ThemeProvider } from '@emotion/react';
+import { GlobalStyle, theme } from '@/styles/theme';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Global styles={GlobalStyle} />
       {children}
-    </>
+    </ThemeProvider>
   );
 }
